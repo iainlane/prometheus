ca4918999cce7ed6c4200e19e9cb63b3ebf1d57c
Robert Fratto <robert.fratto@grafana.com>
2020-03-23 13:44:31 -0400

    share string interner between scraper and remote_write
    This commit exposes the string interner globally with a shared pool and
    modifies both the scraper and remote_write to use that shared pool.
    
    Note that the string_interner_zero_reference_releases_total now has a
    different namespace and subsystem from what it was initially set to in
    the RW code.
    
    Signed-off-by: Robert Fratto <robert.fratto@grafana.com>

--- scrape/scrape.go
+++ scrape/scrape.go
@@ -38,6 +38,7 @@ import (
 
 	"github.com/prometheus/prometheus/config"
 	"github.com/prometheus/prometheus/discovery/targetgroup"
+	"github.com/prometheus/prometheus/pkg/intern"
 	"github.com/prometheus/prometheus/pkg/labels"
 	"github.com/prometheus/prometheus/pkg/pool"
 	"github.com/prometheus/prometheus/pkg/relabel"
@@ -699,6 +700,7 @@ func (c *scrapeCache) iterDone(flushCache bool) {
 		// that haven't appeared in the last scrape.
 		for s, e := range c.series {
 			if c.iter != e.lastIter {
+				intern.ReleaseLabels(intern.Global, e.lset)
 				delete(c.series, s)
 			}
 		}
@@ -1184,6 +1186,8 @@ loop:
 				break loop
 			}
 
+			intern.InternLabels(intern.Global, lset)
+
 			var ref uint64
 			ref, err = app.Add(lset, t, v)
 			switch errors.Cause(err) {
@@ -1366,6 +1370,7 @@ func (sl *scrapeLoop) addReportSample(app storage.Appender, s string, t int64, v
 	ref, err := app.Add(lset, t, v)
 	switch errors.Cause(err) {
 	case nil:
+		intern.InternLabels(intern.Global, lset)
 		sl.cache.addRef(s, ref, lset, hash)
 		return nil
 	case storage.ErrOutOfOrderSample, storage.ErrDuplicateSampleForTimestamp:
